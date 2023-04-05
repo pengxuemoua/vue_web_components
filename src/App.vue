3c3039adecf9b13f3d43db85e6c30b26d3ae7065
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2023-04-05 16:05:02 -0500

    Completed code

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,51 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+
+    <h1>Would you rather?...</h1>
+
+    <would-you-rather 
+    v-bind:question='wyrQuestion'
+    v-bind:answer1='wyrAnswer1'
+    v-bind:answer2='wyrAnswer2'
+    v-on:answer-changed="answerChanged"
+    ></would-you-rather>
+
+    <p>{{ userSelectionMessage }}</p>
+
+  </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import WouldYouRather from './components/WouldYouRather.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    WouldYouRather
+  },
+  data() {
+    return {
+      wyrQuestion: 'Would you rather be an incredibly fast swimmer or an incredibily fast runner?',
+      wyrAnswer1: 'Fast swimmer',
+      wyrAnswer2: 'Fast runner',
+      userSelectionMessage: ''
+    }
+  },
+  methods: {
+    answerChanged(choice){
+      this.userSelectionMessage = `Thanks! You choose ${choice}`
+    }
   }
 }
 </script>
 
 <style>
+
+body{
+  background: tan;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -22,5 +53,6 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  background: turquoise;
 }
 </style>
